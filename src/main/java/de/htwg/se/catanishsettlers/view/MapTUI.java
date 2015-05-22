2ec4445837a0bb7def5a95806779673add8ca724
JonnyTieM <JonnyTieM@users.noreply.github.com>
2015-05-22 13:21:19 +0200

    Start of MapTUI which converts the Map to a String

---
+++ src/main/java/de/htwg/se/catanishsettlers/view/MapTUI.java
@@ -0,0 +1,13 @@
+package de.htwg.se.catanishsettlers.view;
+
+import de.htwg.se.catanishsettlers.model.map.Map;
+
+/**
+ * Created by Jonathan on 22.05.2015.
+ */
+public class MapTUI {
+    Map map;
+    public MapTUI(Map map) {
+        this.map = map;
+    }
+}
