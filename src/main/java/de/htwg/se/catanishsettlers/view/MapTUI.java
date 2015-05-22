1c38e8ce394d8e2f7b056cc6fcb437bad58a8727
JonnyTieM <JonnyTieM@users.noreply.github.com>
2015-05-22 13:37:11 +0200

    basic structure for MapTUI

--- src/main/java/de/htwg/se/catanishsettlers/view/MapTUI.java
+++ src/main/java/de/htwg/se/catanishsettlers/view/MapTUI.java
@@ -1,5 +1,6 @@
 package de.htwg.se.catanishsettlers.view;
 
+import de.htwg.se.catanishsettlers.model.Config;
 import de.htwg.se.catanishsettlers.model.map.Map;
 
 /**
@@ -9,5 +10,37 @@ public class MapTUI {
     Map map;
     public MapTUI(Map map) {
         this.map = map;
+    }
+
+    public String printMap() {
+        String s = "";
+        for (int i = 0; i < Config.FIELDS_HEIGHT; i++) {
+            s += printFirstLine(i);
+            s += printSecondLine(i);
+            s += printThirdLine(i);
+            s += printFourthLine(i);
+        }
+        s += printFirstLine(Config.FIELDS_HEIGHT);
+        return s;
+    }
+
+    private String printFirstLine(int depth) {
+        String s = "";
+        return s;
+    }
+
+    private String printSecondLine(int depth) {
+        String s = "";
+        return s;
+    }
+
+    private String printThirdLine(int depth) {
+        String s = "";
+        return s;
+    }
+
+    private String printFourthLine(int depth) {
+        String s = "";
+        return s;
     }
 }
