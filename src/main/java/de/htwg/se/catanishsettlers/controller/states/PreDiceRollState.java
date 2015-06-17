1ab25460b1f6a429379cbc7e924b711b9cf8c454
JonnyTieM <JonnyTieM@users.noreply.github.com>
2015-06-17 16:29:09 +0200

    removed DiceRollState, since it's not needed since you roll the dice, when you switch from preDiceRollState to PostDiceRollState

--- src/main/java/de/htwg/se/catanishsettlers/controller/states/PreDiceRollState.java
+++ src/main/java/de/htwg/se/catanishsettlers/controller/states/PreDiceRollState.java
@@ -2,6 +2,7 @@ package de.htwg.se.catanishsettlers.controller.states;
 
 import de.htwg.se.catanishsettlers.controller.Game;
 import de.htwg.se.catanishsettlers.controller.IGameState;
+import de.htwg.se.catanishsettlers.model.mechanic.DiceRoll;
 
 /**
  * Created by Stephan on 05.06.2015.
@@ -12,6 +13,19 @@ public class PreDiceRollState implements IGameState {
     }
 
     public void nextState(Game game) {
-        game.setState(new DiceRollState());
+
+        DiceRoll dice = new DiceRoll(2);
+        int rolledNumber = dice.getValue();
+        game.setLastRolledDiceNumber(rolledNumber);
+        distributeResources(rolledNumber, game);
+
+        game.setState(new PostDiceRollState());
+    }
+
+    private void distributeResources(int rolledNumber, Game game) {
+        if (rolledNumber == 7) {
+            return;
+        }
+        game.distributeResources(rolledNumber);
     }
 }
