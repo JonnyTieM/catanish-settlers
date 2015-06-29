b1a19002d3525f65ff106ad1eb63363e9881767b
JonnyTieM <JonnyTieM@users.noreply.github.com>
2015-06-29 15:00:04 +0200

    started with JUnit4 tests for package mechanic

---
+++ src/test/java/de/htwg/se/catanishsettlers/model/mechanic/DiceTest.java
@@ -0,0 +1,52 @@
+package de.htwg.se.catanishsettlers.model.mechanic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Jonathan on 29.06.2015.
+ */
+public class DiceTest {
+    Dice dice;
+
+    @Before
+    public void setUp() throws Exception {
+        dice = new Dice(2,6);
+    }
+
+    @Test
+    public void testRoll() throws Exception {
+        boolean thisWorks = false;
+        dice.roll();
+        int rolledValue = dice.getValue();
+
+        for (int i = 0; i < 100000; i++) {
+            dice.roll();
+            if (rolledValue != dice.getValue()) {
+                thisWorks = true;
+                break;
+            }
+        }
+
+        assertTrue(thisWorks);
+    }
+
+    @Test
+    public void testGetValue() throws Exception {
+        dice.roll();
+        assertEquals(dice.getSingleValues()[0] + dice.getSingleValues()[1], dice.getValue());
+    }
+
+    @Test
+    public void testGetSingleValues() throws Exception {
+        dice.roll();
+        assertEquals(dice.getValue(), dice.getSingleValues()[0] + dice.getSingleValues()[1]);
+    }
+
+    @Test
+    public void testGetDiceCount() throws Exception {
+        assertEquals(2, dice.getDiceCount());
+    }
+}
---
+++ src/test/java/de/htwg/se/catanishsettlers/model/mechanic/InvestmentTest.java
@@ -0,0 +1,25 @@
+package de.htwg.se.catanishsettlers.model.mechanic;
+
+import de.htwg.se.catanishsettlers.model.constructions.City;
+import de.htwg.se.catanishsettlers.model.resources.ResourceCollection;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Jonathan on 29.06.2015.
+ */
+public class InvestmentTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCOST() throws Exception {
+        Investment investment = new City(new Player("Hans"));
+        assertTrue(investment.getCOST().compareTo(new ResourceCollection(0,0,0,2,3)) == 0);
+    }
+}
