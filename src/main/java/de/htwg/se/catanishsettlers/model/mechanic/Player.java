466d11587e42f4f33125b1618bc1cbb9325a15f1
Schmork <somestrangeguy@somebizarrepla.net>
2015-07-01 11:45:24 +0200

    fixed for merge

--- src/main/java/de/htwg/se/catanishsettlers/model/mechanic/Player.java
+++ src/main/java/de/htwg/se/catanishsettlers/model/mechanic/Player.java
@@ -2,12 +2,10 @@ package de.htwg.se.catanishsettlers.model.mechanic;
 
 import de.htwg.se.catanishsettlers.model.Config;
 import de.htwg.se.catanishsettlers.model.resources.ResourceCollection;
-import de.htwg.se.catanishsettlers.view.gui.MainFrame.ColorChoosePanel;
 
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Observable;
-import java.util.Observer;
 
 /**
  * Created by Stephan on 31.03.2015.
