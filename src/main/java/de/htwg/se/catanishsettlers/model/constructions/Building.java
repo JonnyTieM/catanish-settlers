acbe6246bb6cba3fdf06195f9843996271e4452f
JonnyTieM <JonnyTieM@users.noreply.github.com>
2015-07-02 18:27:48 +0200

    removed collectedResources from Building, cause it wasn't used anymore

--- src/main/java/de/htwg/se/catanishsettlers/model/constructions/Building.java
+++ src/main/java/de/htwg/se/catanishsettlers/model/constructions/Building.java
@@ -9,23 +9,10 @@ import de.htwg.se.catanishsettlers.model.resources.ResourceCollection;
 public abstract class Building extends Construction{
     //public static int SCORE;    // how many victory points? //this field not needed. look it up in Config
     protected int yield;    // how many resources gathered?
-    private ResourceCollection collectedResources;    // temporal storage for resources from fields
 
     public Building(Player player) {
         super(player);
-        collectedResources = new ResourceCollection();
     }
 
     public int getYield() { return yield; }
-
-//    public ResourceCollection yieldResources() {
-//        ResourceCollection collectedResources = new ResourceCollection();
-//
-//        Vertex vertex = (Vertex)getPosition();
-//        for(Field surroundingField : vertex.getSurroundingFields()) {
-//            EResource resource = surroundingField.getResourceType();
-//            collectedResources.add(resource, yield);
-//        }
-//        return collectedResources;
-//    }
 }
